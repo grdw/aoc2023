7771f39acf7a1a4d48d4de5b20c81510ae1de44e
grdw <gerard@grdw.nl>
2023-12-04 10:37:16 +0100

    Resolve d4p1

---
+++ p04/src/main.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::ops::Range;
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    winning_numbers: Vec<u16>,
+    my_numbers: Vec<u16>
+}
+
+fn main() {
+    let cards = parse("input");
+    println!("Part 1: {:?}", part1(&cards));
+    println!("Part 2: {:?}", part2(&cards));
+}
+
+fn part1(cards: &[Card]) -> u32 {
+    cards.iter().map(|card| {
+        let mut n = 0;
+        for w in &card.winning_numbers {
+            if card.my_numbers.contains(&w) {
+                // if ugly, then ugly
+                if n == 0 {
+                    n = 1
+                } else {
+                    n *= 2
+                }
+            }
+        }
+
+        n
+    }).sum()
+}
+
+#[test]
+fn test_part1() {
+    let cards = parse("test_input");
+    let n = part1(&cards);
+
+    assert_eq!(n, 13);
+}
+
+fn part2(card: &[Card]) -> u32 {
+    return 0
+}
+
+#[test]
+fn test_part2() {
+    let cards = parse("test_input");
+    let n = part2(&cards);
+
+    assert_eq!(n, 1);
+}
+
+fn to_u16_vec(string: &str) -> Vec<u16> {
+    string
+        .split_whitespace()
+        .map(|n| n.parse::<u16>().unwrap())
+        .collect()
+}
+
+fn parse(input: &'static str) -> Vec<Card> {
+    let mut cards = vec![];
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+
+    for (id, line) in reader.lines().enumerate() {
+        if let Ok(l) = line {
+            let (_, numbers) = l.split_once(": ").unwrap();
+            let (winning, my) = numbers.split_once(" | ").unwrap();
+
+            cards.push(
+                Card {
+                    id: id + 1,
+                    winning_numbers: to_u16_vec(winning),
+                    my_numbers: to_u16_vec(my)
+                }
+            );
+        }
+    }
+
+    cards
+}
+
+#[test]
+fn test_parse() {
+    let cards = parse("test_input");
+    assert_eq!(cards[0].my_numbers.len(), 8);
+    assert_eq!(cards[0].winning_numbers.len(), 5);
+}
