90a3f6b12ac7f0e2ab91999209f47994a8a22c26
Gerard <gerard@wetransfer.com>
2023-12-15 12:48:56 +0100

    Solve d15

--- p15/src/main.rs
+++ p15/src/main.rs
@@ -1,15 +1,66 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let list = parse("input");
     println!("p1: {}", sum(&list));
+    println!("p2: {}", focussing_power(&list));
 }
 
 const MAX_FACTOR: u16 = 17;
 const REMAINDER: u16 = 256;
 
 fn sum(l: &Vec<String>) -> u32 {
-    l.iter().map(|n| hash_string(&n) as u32 ).sum()
+    l.iter().map(|n| hash_string(&n) as u32).sum()
+}
+
+fn focussing_power(l: &Vec<String>) -> u32 {
+    let mut map: HashMap<u16, Vec<(&str, u16)>> = HashMap::new();
+
+    for n in l {
+        let (action, label, focal_length) = match n.split_once("=") {
+            Some(label) => {
+                ("add", label.0, label.1.parse::<u16>().unwrap())
+            }
+            None => {
+                let (label, _) = n.split_once("-").unwrap();
+                ("remove", label, 0)
+            }
+        };
+
+        let bnr = hash_string(label);
+        let entry = map.entry(bnr);
+
+        match action {
+            "add" => {
+                entry
+                    .and_modify(|m| {
+                        match m.iter().position(|(x, _)| *x == label) {
+                            Some(x) => {
+                                m.remove(x);
+                                m.insert(x, (&label, focal_length));
+                            },
+                            None => m.push((&label, focal_length))
+                        }
+                    })
+                    .or_insert(vec![(label, focal_length)]);
+            },
+            "remove" => {
+                entry.and_modify(|m| {
+                    if let Some(x) = m.iter().position(|(x, _)| *x == label) {
+                        m.remove(x);
+                    }
+                });
+            },
+            _ => panic!("wrong action buddy")
+        }
+    }
+
+    map.iter().map(|(k, v)| {
+        v.iter().enumerate().map(|(i, m)| {
+            ((k + 1) * (i + 1) as u16 * m.1) as u32
+        }).sum::<u32>()
+    }).sum::<u32>()
 }
 
 fn hash(current_value: &mut u16, c: char) -> u16 {
@@ -45,6 +96,11 @@ fn test_hash_string() {
 #[test]
 fn test_sum() {
     let list = parse("test_input");
-    println!("{:?}", list);
     assert_eq!(sum(&list), 1320);
+}
+
+#[test]
+fn test_focussing_power() {
+    let list = parse("test_input");
+    assert_eq!(focussing_power(&list), 145);
 }
